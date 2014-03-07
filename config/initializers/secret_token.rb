f5d647a1b55ea6898a3a16e86a15c9231ba51b71
vlad_kushner <vladkushner@gmail.com>
2014-03-07 19:01:07 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewDebot::Application.config.secret_key_base = '4dff050d2ab20de240385cf72d35b1f4e33d8b6402d0095edeab3985a9253c1465e24c2ab6343a98cd32268be931411950fc8d8559d038f377185a43da558656'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewDebot::Application.config.session_store :cookie_store, key: '_new_debot_session'
